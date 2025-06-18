485285f10072d66f9e1fa78669b51dc27803a28f
jelix72 <inmune@gmail.com>
2025-06-18 07:39:10 -0300

    Creando entidades para el proyecto

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,22 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'app_conference')]
-    public function index(): Response
+    #[Route('/hello/{name}', name: 'conference')]
+    public function index(string $name): Response
     {
-        return new Response(
-            '<html><body>
-                <img src="/images/under-construction.png" alt="Symfony Under Contruction" />
-            </body></html>'
-        ); 
-
-        // return $this->render('conference/index.html.twig', [
-        //     'controller_name' => 'ConferenceController',
-        // ]);
+        return $this->render('conference/index.html.twig', [
+            'name' => $name,
+        ]);
     }
 }
